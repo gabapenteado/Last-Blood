549dd7631aa9153c7ec5d07a4a64b08df776d5f6
Blanes <marcelo@elementalstudio.com.br>
2013-09-20 16:20:02 -0300

    CameraFollower.cs criado e funcional

---
+++ Assets/Scripts/Scenario/CameraFollower.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour 
+{
+	public Transform target;
+	public float scenarioMinValue = 0f;
+	public float scenarioMaxValue = 300f;
+	float myXPos;
+	float newXPos;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		myXPos = this.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if(!target) return;
+		
+		newXPos = target.transform.position.x;
+		newXPos = Mathf.Clamp(newXPos, scenarioMinValue, scenarioMaxValue);
+		if(newXPos != myXPos)
+		{
+			myXPos = newXPos;
+			MoveCamera();		
+		}
+	}
+	void MoveCamera()
+	{
+		transform.position = new Vector3(myXPos,transform.position.y,transform.position.z);
+	}
+}
