6c97a460e9b1925bdcb32bebf927c09a08c7828e
Blanes <marcelo@elementalstudio.com.br>
2013-09-24 16:41:49 -0300

    Slash Effect para o ataque do inimigo

---
+++ Assets/Scripts/Util/DestroyAfterAnim.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterAnim : MonoBehaviour 
+{
+	tk2dSpriteAnimator animator;
+	tk2dSpriteAnimationClip clip;
+	
+	float destroyTime = 0.3f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		if(gameObject.GetComponent<tk2dSpriteAnimator>())
+		{
+			animator = gameObject.GetComponent<tk2dSpriteAnimator>();
+			clip = animator.CurrentClip;
+		}
+	}
+	void LateUpdate()
+	{		
+		if(!animator.IsPlaying(clip))
+		{
+			StartCoroutine("Destroy");
+		}
+	}
+	IEnumerator Destroy()
+	{
+		
+		yield return new WaitForSeconds(destroyTime);
+		Destroy(gameObject);
+	}
+}
